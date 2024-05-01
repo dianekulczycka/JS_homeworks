522f0c2ef404d81d75a7d9dca24cf0231618b026
diane <leshlesh1337@gmail.com>
2024-05-01 12:25:17 -0500

    small fix

--- hw3/js/hw3_additional.js
+++ hw3/js/hw3_additional.js
@@ -2,13 +2,13 @@
 // - з 5 числових значень
 let arrayOfNums = [];
 for (let i = 1; i <= 5; i++) {
-    arrayOfNums.push(i);
+    arrayOfNums[arrayOfNums.length] = i;
 }
 
 // - з 5 стічкових значень
 let arrayOfStrings = [];
 for (let j = 1; j <= 5; j++) {
-    arrayOfStrings.push(`item ${j}`);
+    arrayOfStrings[arrayOfStrings.length] = `item ${j}`;
 }
 
 // - з 5 значень стрічкового, числового та булевого типу
@@ -16,15 +16,17 @@ let randomArray = [];
 for (let k = 1; k <= 5; k++) {
     let random = Math.random() * 10;
     if (random <= 3) {
-        randomArray.push(`i am a string ${k}`);
+        randomArray[randomArray.length] = `i am a string ${k}`;
     } else if (k > 3 && k <= 7) {
-        randomArray.push(k);
+        randomArray[randomArray.length] = k;
     } else {
-        if (k % 2 === 0) {
-            randomArray.push(true);
-        } else {
-            randomArray.push(false);
-        }
+        randomArray[randomArray.length] = k % 2 === 0;
+        // інтелідж не давав пушнути поки я не замінила код нижче на тернарний
+        // if (k % 2 === 0) {
+        //    randomArray[randomArray.length] = true;
+        // } else {
+        //    randomArray[randomArray.length] = false;
+        // }
     }
 }
 // - та вивести його в консоль
