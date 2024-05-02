e3cdd2f127baec39817ea8fe6fde09656babe8ad
diane <leshlesh1337@gmail.com>
2024-05-02 09:36:55 -0500

    small fix

--- hw3/js/hw3_additional.js
+++ hw3/js/hw3_additional.js
@@ -14,19 +14,13 @@ for (let j = 1; j <= 5; j++) {
 // - з 5 значень стрічкового, числового та булевого типу
 let randomArray = [];
 for (let k = 1; k <= 5; k++) {
-    let random = Math.random() * 10;
-    if (random <= 3) {
+    let random = Math.floor(Math.random() * 10);
+    if (random >= 0 && random <= 2) {
         randomArray[randomArray.length] = `i am a string ${k}`;
-    } else if (k > 3 && k <= 7) {
+    } else if (random >= 3 && random <= 6) {
         randomArray[randomArray.length] = k;
-    } else {
-        randomArray[randomArray.length] = k % 2 === 0;
-        // інтелідж не давав пушнути поки я не замінила код нижче на тернарний вираз
-        // if (k % 2 === 0) {
-        //    randomArray[randomArray.length] = true;
-        // } else {
-        //    randomArray[randomArray.length] = false;
-        // }
+    } else if (random > 6 && random <= 9){
+        randomArray[randomArray.length] = random % 2 === 0;
     }
 }
 // - та вивести його в консоль
