21e2eac48cd26cf267ac4560719216221abd8c6d
diane <leshlesh1337@gmail.com>
2024-05-29 10:17:24 -0500

    refactored recipes.js

--- hw11/recipes/recipes.js
+++ hw11/recipes/recipes.js
@@ -2,65 +2,59 @@
 // а вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
 
 let baseURL = 'https://dummyjson.com/recipes';
+const createHTMLEl = (type, value, attributeKey, attributeValue) => {
+    let element = document.createElement(`${type}`);
+    element.innerText = value;
+    if (attributeKey && attributeValue) {
+        element[attributeKey] = attributeValue;
+    }
+    return element;
+}
+const iterator = (array, type, parent) => {
+    for (const el in array) {
+        parent.appendChild(createHTMLEl(type, array[el]));
+    }
+}
 fetch(baseURL)
     .then(response => response.json())
     .then(({recipes}) => {
-        console.log(recipes);
         for (const recipe in recipes) {
-            let recipeContainer = document.createElement("div");
-            let recipeHeader = document.createElement("h1");
-            recipeHeader.innerText = `${recipes[recipe].id}. ${recipes[recipe].name}`;
-            let rating = document.createElement("h2");
-            rating.innerText = `Rating: ${recipes[recipe].rating} based on ${recipes[recipe].reviewCount} reviews`;
-            let mainInfo = document.createElement("h3");
-            mainInfo.innerText = `Cuisine: ${recipes[recipe].cuisine}
+            //
+            // main fields
+            //
+            let recipeHeader = createHTMLEl('h1', `${recipes[recipe].id}. ${recipes[recipe].name}`)
+            let rating = createHTMLEl('h2', `Rating: ${recipes[recipe].rating} based on ${recipes[recipe].reviewCount} reviews`);
+            let mainInfo = createHTMLEl("h3", `Cuisine: ${recipes[recipe].cuisine}
             Difficulty: ${recipes[recipe].difficulty}
-            Author: userID ${recipes[recipe].userId}`;
-            let desc = document.createElement("h4");
-            desc.innerText = `Calories per serving: ${recipes[recipe].caloriesPerServing} 
+            Author: userID ${recipes[recipe].userId}`);
+            let desc = createHTMLEl("h4", `Calories per serving: ${recipes[recipe].caloriesPerServing} 
             Cooking time: ${recipes[recipe].cookTimeMinutes} minutes
             Preparation time: ${recipes[recipe].prepTimeMinutes} minutes
             Type of meal: ${recipes[recipe].mealType}
             Servings:  ${recipes[recipe].servings}
-            `;
-            let image = document.createElement("img");
-            image.src = recipes[recipe].image;
+            `);
+            let image = createHTMLEl("img", "", "src", recipes[recipe].image);
+            //
+            // appending to doc
+            //
+            document.getElementById("recipes").append(image, recipeHeader, rating, mainInfo, desc);
+            //
+            // instructions arr iteration
+            //
+            document.getElementById("recipes").appendChild(createHTMLEl("h3", "Instructions:"));
+            iterator(recipes[recipe].instructions, "li", document.getElementById("recipes"));
+            //
+            // ingredients arr iteration
+            //
+            let ingredientsContainerHeader = createHTMLEl("h3", "Ingredients:");
+            document.getElementById("recipes").appendChild(ingredientsContainerHeader);
+            iterator(recipes[recipe].ingredients, "li", document.getElementById("recipes"));
+            //
+            // tags arr iteration
+            //
+            document.getElementById("recipes").appendChild(createHTMLEl("h3", "Tags:"));
+            iterator(recipes[recipe].tags, "li", document.getElementById("recipes"));
 
-            let instructionsContainer = document.createElement("ul");
-            let instructionsContainerHeader = document.createElement("h3");
-            instructionsContainerHeader.innerText = "Instructions:";
-            instructionsContainer.appendChild(instructionsContainerHeader);
-            let instructionsArr = recipes[recipe].instructions;
-            for (const instruction of instructionsArr) {
-                let instructionEl = document.createElement("li");
-                instructionEl.innerText = instruction;
-                instructionsContainer.appendChild(instructionEl);
-            }
-
-            let ingredientsContainer = document.createElement("div");
-            let ingredientsContainerHeader = document.createElement("h3");
-            ingredientsContainerHeader.innerText = "Ingredients:";
-            ingredientsContainer.appendChild(ingredientsContainerHeader);
-            let ingredientsArr = recipes[recipe].ingredients;
-            for (const ingredient of ingredientsArr) {
-                let ingredientEl = document.createElement("li");
-                ingredientEl.innerText = ingredient;
-                ingredientsContainer.appendChild(ingredientEl);
-            }
-
-            let tagsContainer = document.createElement("div");
-            let tagsContainerHeader = document.createElement("h3");
-            tagsContainerHeader.innerText = "Tags:";
-            tagsContainer.appendChild(tagsContainerHeader);
-            let tagsArr = recipes[recipe].tags;
-            for (const tag of tagsArr) {
-                let tagEl = document.createElement("li");
-                tagEl.innerText = tag;
-                tagsContainer.appendChild(tagEl);
-            }
-
-            recipeContainer.append(image, recipeHeader, rating, mainInfo, desc, ingredientsContainer, instructionsContainer, tagsContainer);
-            document.getElementById("recipes").appendChild(recipeContainer);
         }
 
     });
